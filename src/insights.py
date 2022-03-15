3d09394fa29708366e6a12ed58645c21dbfa2d5f
Wadson92 <wadson.dev92@gmail.com>
2022-03-15 09:38:24 -0300

    Requisitos 8 e 9 ok

--- src/insights.py
+++ src/insights.py
@@ -60,44 +60,28 @@ print(get_min_salary("src/jobs.csv"))
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError("min_salary or max_salary is missing")
+    if (
+        not isinstance(job["min_salary"], int) or
+        not isinstance(job["max_salary"], int)
+    ):
+        raise ValueError("min_salary and max_salary must be valid integers")
+    if int(job["min_salary"]) > int(job["max_salary"]):
+        raise ValueError("min salary is greater than max_salary")
+    if not isinstance(salary, int):
+        raise ValueError("salary must be valid integer")
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    filter_salary = []
+    for job in jobs:
+        try:
+            matches_salary_range(job, salary)
+        except ValueError:
+            pass
+        else:
+            if matches_salary_range(job, salary):
+                filter_salary.append(job)
+    return filter_salary
